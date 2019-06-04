433e184c33593891713149ee36cb5d1a5d303514
Pavillin <dylansprague11@hotmail.com>
2019-06-04 19:35:47 -0400

    added articles route and fixed nodemon dev script

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ db.once('open', () => console.log('Connection to mongoose successful'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
