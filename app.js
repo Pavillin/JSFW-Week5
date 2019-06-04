575b822f2fcad8db2781ec20cdab943b7be722fe
Pavillin <dylansprague11@hotmail.com>
2019-06-04 18:57:36 -0400

    mongoose connection setup

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://test:test123@jsfw-class-ejvhe.mongodb.net/test?retryWrites=true&w=majority`, {
+  useNewUrlParser: true //remove depreciated warning
+});
+
+var db = mongoose.connection;
+db.on('error', err => console.error(err));
+db.once('open', () => console.log('Connection to mongoose successful'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
