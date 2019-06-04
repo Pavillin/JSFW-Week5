0c4cdce0e16483ececf337d934edc7b3cc8b81f7
Pavillin <dylansprague11@hotmail.com>
2019-06-04 19:49:54 -0400

    Created details and list page

--- routes/articles.js
+++ routes/articles.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET articles listing. */
-router.get('/', function(req, res) {
-  res.send('Working');
+router.get('/', async(req, res) => {
+  const articles = await Article.find();
+
+  //res.json(articles); //respond back with our articles in json format
+  res.render('articles/list', {articles});
 });
 
 module.exports = router;
